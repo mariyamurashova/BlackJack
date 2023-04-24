58c894a8e32d86129e556041fe016ef9528a92d4
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-04-24 23:39:32 +0200

    Update player.rb

--- player.rb
+++ player.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Player
-  attr_accessor :hand, :money, :name
+  attr_accessor :hand, :money, :name, :turn, :sum_hand, :has_A
 
   def initialize(name, money = 100)
     @name = name
     @hand = []
     @money = money
-    @next_step = false
+    @turn = false
+    @has_A = false
   end
 
   def show_cards
@@ -17,10 +18,32 @@ class Player
 
   def deal_card(deck, num)
     num.times do
-      choosen_card = deck.sample
-      deck.delete(choosen_card)
-      @hand << choosen_card
+      card_hand = deck.sample
+      deck.delete(card_hand)
+      has_Ace(card_hand)
+      @hand << card_hand
     end
+  end
+
+  def has_Ace(card)
+    return unless card.name.include?('A')
+
+    if @has_A == false
+      card.value = 11
+      @has_A = true
+    else
+      card.value = 1
+    end
+  end
+
+  def calculate_amount
+    @sum_hand = 0
+    @hand.each { |card| @sum_hand += card.value }
+    puts "#{name} your sum is #{@sum_hand}"
+  end
+
+  def skip
+    @turn = true
   end
 
   def current_amount
